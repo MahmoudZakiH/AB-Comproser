fd26622e3f56cb72daf647df8734413d6f2169c1
osama <osama@gmail.com>
2026-01-27 14:43:07 +0200

    edit is storable

---
+++ ibs_is_storable/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Is Storable',
+    'version': '18.0.1.0',
+    'author': 'IBS',
+    'depends': ['base','product'],
+    'data': [
+        "views/product_view.xml",
+    ],
+    'sequence':-100,
+    'application': True,
+    'license': 'LGPL-3',
+}
